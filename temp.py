83154e84407220ab002dd31d068fbf99887ffb50
Doyeon Kim <alfine122299@gmail.com>
2025-12-12 14:38:24 +0900

    chore : seperate leetcode

--- temp.py
+++ temp.py
@@ -1,1 +1,21 @@
-A, B = map(int, input().split())
+import sys
+input = sys.stdin.readline
+'''
+입력받은거 기반 큰수로 이중for문 -> 리스트만들기
+'''
+
+A, B = map(int, input().split(' '))
+small, big = min(A,B) , max(A,B)
+mylist = []
+for i in range(1, big//4+2):
+    mylist.append([(i*4-3),(i*4-2),(i*4-1),(i*4)])
+
+answer_list = []
+for i in range(big//4+1):
+    for k in range(4):
+        if mylist[i][k] == small:
+            answer_list.append([i, k])
+        if mylist[i][k] == big:
+            answer_list.append([i, k])
+
+print(answer_list)
